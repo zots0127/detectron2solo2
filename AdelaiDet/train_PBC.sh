f0d0cbd154bb1903868c4fd823b70f606df19d1d
zots0127 <zots0127@gmail.com>
2021-10-20 18:16:55 +0900

    l

---
+++ AdelaiDet/train_PBC.sh
@@ -0,0 +1,4 @@
+OMP_NUM_THREADS=1 python tools/train_PBC_solo.py \
+    --config-file configs/SOLOv2/R50_3x_PBC.yaml \
+    --num-gpus 1 \
+    OUTPUT_DIR training_dir/SOLOv2_R50_3x_PBC
--- AdelaiDet/train_PCM.sh
+++
@@ -1,4 +0,0 @@
-OMP_NUM_THREADS=1 python tools/train_PBC_solo.py \
-    --config-file configs/SOLOv2/R50_3x_PCM.yaml \
-    --num-gpus 1 \
-    OUTPUT_DIR training_dir/SOLOv2_R50_3x_PCM
